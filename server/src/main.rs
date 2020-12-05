bbd814fd23f29f9b89dc9b36014dd57940370352
荀徒之 <kalxd@126.com>
2020-12-05 20:40:53 +0800

    最最基本的网络服务。

---
+++ server/src/main.rs
@@ -0,0 +1,14 @@
+use actix_web::{get, App, HttpServer, Responder};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    "hello world"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:3000")?
+        .run()
+        .await
+}
