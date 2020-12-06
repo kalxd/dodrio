98711b4c46d03a7fbcbee6fd1f45f91c3d2d7898
荀徒之 <kalxd@126.com>
2020-12-06 19:10:29 +0800

    rust fmt

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,13 +2,13 @@ use actix_web::{get, App, HttpServer, Responder};
 
 #[get("/")]
 async fn index() -> impl Responder {
-    "hello world"
+	"hello world"
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(index))
-        .bind("127.0.0.1:3000")?
-        .run()
-        .await
+	HttpServer::new(|| App::new().service(index))
+		.bind("127.0.0.1:3000")?
+		.run()
+		.await
 }
