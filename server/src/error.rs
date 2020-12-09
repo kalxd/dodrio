96766f1b02cd0f6c969a3959500ab18887a82838
荀徒之 <kalxd@126.com>
2020-12-09 18:44:27 +0800

    保留数据唯一约束错误。

--- server/src/error.rs
+++ server/src/error.rs
@@ -7,6 +7,8 @@ pub type Res<T> = std::result::Result<T, Error>;
 /// 业务错误类型。
 #[derive(Debug)]
 pub enum Error {
+	/// 内部数据重复，常用于数据库操作。
+	Duplicate,
 	/// 内部服务错误。
 	ServerE(String),
 }
@@ -24,6 +26,7 @@ where
 impl std::fmt::Display for Error {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		let msg = match self {
+			Self::Duplicate => "重复操作",
 			Self::ServerE(msg) => msg,
 		};
 
@@ -34,6 +37,7 @@ impl std::fmt::Display for Error {
 impl ResponseError for Error {
 	fn status_code(&self) -> StatusCode {
 		match self {
+			Self::Duplicate => StatusCode::INTERNAL_SERVER_ERROR,
 			Self::ServerE(_) => StatusCode::INTERNAL_SERVER_ERROR,
 		}
 	}
--- server/src/helper/db.rs
+++ server/src/helper/db.rs
@@ -1,7 +1,7 @@
 //! 第三方库业务上的封装。
 use anyhow::Result;
 use deadpool_postgres::Pool;
-use tokio_postgres::{row::Row, types::ToSql, ToStatement};
+use tokio_postgres::{error::SqlState, row::Row, types::ToSql, ToStatement};
 
 use std::convert::{TryFrom, TryInto};
 
@@ -11,14 +11,31 @@ use crate::error::{Error, Res};
 pub struct DB(Pool);
 
 impl DB {
+	// 低等级查询函数，处理部分错误。
+	async fn raw_query<T>(&'_ self, statement: &'_ T, params: &'_ [&'_ (dyn ToSql + '_ + Sync)]) -> Res<Vec<Row>>
+	where
+		T: ToStatement + ?Sized,
+	{
+		let client = self.get().await?;
+		client.query(statement, params).await.map_err(|e| {
+			if e.code() == Some(&SqlState::UNIQUE_VIOLATION) {
+				return Error::Duplicate;
+			} else {
+				return e.into();
+			}
+		})
+	}
+
 	pub async fn query<T, R>(&'_ self, statement: &'_ T, params: &'_ [&'_ (dyn ToSql + '_ + Sync)]) -> Res<Vec<R>>
 	where
 		T: ToStatement + ?Sized,
 		R: TryFrom<Row, Error = Error>,
 	{
-		let client = self.get().await?;
-		let rows = client.query(statement, params).await?;
-		rows.into_iter().map(TryFrom::try_from).collect()
+		self.raw_query(statement, params)
+			.await?
+			.into_iter()
+			.map(TryFrom::try_from)
+			.collect()
 	}
 
 	pub async fn query_<T, R>(&'_ self, statement: &'_ T) -> Result<Vec<R>>
@@ -39,8 +56,7 @@ impl DB {
 		T: ToStatement + ?Sized,
 		R: TryFrom<Row, Error = Error>,
 	{
-		let client = self.get().await?;
-		let mut rows = client.query(statement, params).await?;
+		let mut rows = self.raw_query(statement, params).await?;
 
 		rows.pop().map(TryInto::try_into).transpose()
 	}
@@ -58,8 +74,7 @@ impl DB {
 	where
 		T: ToStatement + ?Sized,
 	{
-		let client = self.get().await?;
-		let mut rows = client.query(statement, params).await?;
+		let mut rows = self.raw_query(statement, params).await?;
 
 		let r = rows.pop().map(|row| row.get(0)).unwrap_or(false);
 		Ok(r)
