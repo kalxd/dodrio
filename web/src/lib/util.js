0014807d4ec4892403b46ffe3e8d7e2c0b80aa19
荀徒之 <kalxd@126.com>
2021-01-11 14:41:56 +0800

    删除noop。

--- web/src/lib/util.js
+++ web/src/lib/util.js
@@ -14,12 +14,6 @@ const _fmap = (...args) => {
 };
 
 /**
- * noop :: a -> ()
- * 什么也不做的函数。
- */
-export function noop(_) {}
-
-/**
  * fmap :: (a -> z) -> Maybe a -> Maybe z
  */
 export const fmap = R.curryN(2, _fmap);
