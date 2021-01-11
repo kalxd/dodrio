62eb37403643a2ba850d79593f031a1878b35e61
荀徒之 <kalxd@126.com>
2021-01-11 10:37:15 +0800

    重新整理注册界面。

--- web/src/Main.jsx
+++ web/src/Main.jsx
@@ -4,13 +4,17 @@ import Placeholder from "./lib/UI/Placeholder";
 import Error from "./lib/UI/Error";
 import useError from "./lib/Hook/error";
 
+import Setup from "./Setup/Main";
+
 export default function Main() {
 	const error = useError();
 
 	return (
 		<div className="ui container">
 			<Error error={error} />
-			<Placeholder />
+			{/* <Placeholder /> */}
+
+			<Setup />
 		</div>
 	);
 }
--- web/src/Setup/Main.jsx
+++ web/src/Setup/Main.jsx
@@ -1,46 +1,10 @@
 import React, { useState } from "react";
-import {
-	Alert,
-	Form,
-	Button,
-	Input,
-	Divider,
-	Typography
-} from "antd";
+import * as R from "rambda";
+import Form, { Field } from "rc-field-form";
 
 import struct from "../lib/struct";
-import { noop } from "../lib/util";
-import { SiteInfoType } from "../lib/t";
-import fetch, { jsonHeader } from "../lib/shttp";
-
 import Error from "../lib/UI/Error";
-
-const BOX_STYLE = {
-	width: "60%",
-	margin: "60px auto"
-}
-
-const LAYOUT = {
-	labelCol: {
-		span: 6
-	},
-	wrapperCol: {
-		span: 18
-	}
-};
-
-const OFFSET_LAYOUT =  {
-	wrapperCol: {
-		offset: 6,
-		span: 18
-	}
-};
-
-const ITEM_RULE = [
-	{
-		required: true
-	}
-];
+import useError from "../lib/Hook/error";
 
 const SiteType = struct(
 	// String
@@ -62,94 +26,62 @@ const SiteType = struct(
 function Setup(prop) {
 	const { onRegist } = prop;
 
-	const [form] = Form.useForm();
-	const [err, setErr] = useState(null);
-
-	// finish :: SiteType -> ()
-	const finish = site => {
-		const body = SiteType.toJSON(site);
-
-		const init = {
-			headers: jsonHeader,
-			method: "POST",
-			body: JSON.stringify(body)
-		};
-
-		fetch("/_/admin/regist", init)
-			.then(SiteInfoType.fromJSON)
-			.then(onRegist)
-			.catch(setErr)
-		;
-	};
+	const error = useError();
 
 	return (
-		<div style={BOX_STYLE}>
-			<Typography.Title>
-				网站还需要一些基本设置，请填写以下信息
-			</Typography.Title>
+		<div className="ui container">
+			<Form className="ui form">
+				<div className="ui message">
+					<div className="header">
+						马上完成！
+					</div>
+					<p>
+						请填写网站基本信息、创建后台管理员。
+					</p>
+				</div>
 
-			<Error error={err} />
+				<Error error={error} />
 
-			<Form {...LAYOUT}
-				  form={form}
-				  onFinish={finish}
-			>
-				<Form.Item {...OFFSET_LAYOUT}>
-					<Alert message="一些网站描述" />
-				</Form.Item>
+				<div className="field required">
+					<label>网站名称</label>
+					<Field name="title">
+						<input placeholder="网站名称" />
+					</Field>
+				</div>
 
-				<Form.Item
-					label="网站信息"
-					name="title"
-					required
-					rules={ITEM_RULE}
-				>
-					<Input placeholder="网站的主题，让人眼前一亮、记忆深刻的名字。" />
-				</Form.Item>
+				<div className="field required">
+					<label>网站描述</label>
+					<Field name="desc">
+						<textarea rows="3" placeholder="网站描述" />
+					</Field>
+				</div>
 
-				<Form.Item
-					label="网站描述"
-					name="desc"
-					required
-					rules={ITEM_RULE}
-				>
-					<Input placeholder="网站的描述、简介。" />
-				</Form.Item>
+				<div className="ui divider" />
 
-				<Divider />
+				<div className="field required">
+					<label>用户名</label>
+					<Field name="username">
+						<input placeholder="管理员用户名" />
+					</Field>
+				</div>
 
-				<Form.Item {...OFFSET_LAYOUT}>
-					<Alert message="新建后台管理员" />
-				</Form.Item>
+				<div className="field required">
+					<label>密码</label>
+					<Field name="password">
+						<input type="password" placeholder="管理员登录密码" />
+					</Field>
+				</div>
 
-				<Form.Item
-					label="用户名"
-					name="username"
-					required
-					rules={ITEM_RULE}
-				>
-					<Input placeholder="管理员用户名。" />
-				</Form.Item>
-
-				<Form.Item
-					label="密码"
-					name="password"
-					required
-					rules={ITEM_RULE}
-				>
-					<Input.Password placeholder="登录密码。" />
-				</Form.Item>
-
-				<Form.Item {...OFFSET_LAYOUT}>
-					<Button type="primary" htmlType="submit">提交</Button>
-				</Form.Item>
+				<div className="field">
+					<button className="ui primary button">提交</button>
+				</div>
 			</Form>
 		</div>
 	);
 }
 
 Setup.defaultProps = {
-	onRegist: noop
+	onRegist: R.identity
 };
 
 export default Setup;
