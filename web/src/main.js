9f17c4222c3c69719a519cb5201c721087418907
荀徒之 <kalxd@126.com>
2021-01-17 15:49:12 +0800

    拍扁。

--- web/src/main.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "react-dom";
-import Main from "./Main.jsx";
-
-import "fomantic-ui-css/semantic.min.css";
-
-const mountNode = document.createElement("main");
-document.body.appendChild(mountNode);
-render(<Main/>, mountNode);
